da4f60686568b680329c1f941457dd1b06f4bdd8
leoleovich <leoleovich@meta.com>
2026-06-16 08:47:08 -0700

    f9_defvalsets: add F9T-10B (L1+L5) config array
    The ZED-F9T-10B supports L1+L5 signals instead of L1+L2. Add a
    dedicated config array (default_configuration_f9t_10b) with the
    correct L5 signal keys:
    
      GPS_L5_ENA, GAL_E5A_ENA, QZSS_L5_ENA = true
      BDS_B2_ENA, GLO_L2_ENA = false (no L5 equivalent)

--- common/f9_defvalsets.h
+++ common/f9_defvalsets.h
@@ -5,4 +5,6 @@ extern const char default_configuration[284][128];
 extern const int default_configuration_size;
 extern const char default_configuration_v220[297][128];
 extern const int default_configuration_v220_size;
+extern const char default_configuration_f9t_10b[297][128];
+extern const int default_configuration_f9t_10b_size;
 #endif
